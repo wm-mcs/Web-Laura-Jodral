00bbf6bb8416456f6d6a40f4bf5ee254ae0dcdf8
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-27 21:39:45 -0300

    sd

--- resources/views/layouts/paginasPersonalizadas/layout.blade.php
+++ resources/views/layouts/paginasPersonalizadas/layout.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700&display=swap" rel="stylesheet">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{url()}}/fonts/icomoon/style.css">
     <link rel="stylesheet" href="{{url()}}/fonts/flaticon/font/flaticon.css">   
