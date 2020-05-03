de5c561402b0eb8525cb3cefd9a7dd9cdd9865f3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-03 19:32:38 -0300

    df

--- resources/views/paginas/paginas_personalizadas/home_contacto.blade.php
+++ resources/views/paginas/paginas_personalizadas/home_contacto.blade.php
@@ -1,10 +1,13 @@
-<div  class="site-section background_img img_contacto-home " id="contacto_home">
+<div  class="p-5 background_img img_contacto-home " id="contacto_home">
   <div class="container">
     <div class="row align-items-center justify-content-center justify-content-lg-end">
       <div class="col-lg-6">
-        
-        <h3 class="titulos-class color-text-primary mb-1">Contáctate ahora</h3>
-        <p class="color-text-gris parrafo-class mb-4">Para contactarte rellenar el formulario de aqui abajo <i class="fas fa-hand-point-down"></i></p>
+        <div class="container">
+          <div class="row">
+            <h3 class="titulos-class text-color-primary mb-1">Contáctate ahora</h3>
+            <p class="color-text-gris parrafo-class mb-4">Para contactarte rellenar el formulario de aqui abajo <i class="fas fa-hand-point-down"></i></p>
+          </div>
+        </div>
         <contacto-component :empresa="empresa" :color="variables.input_color_primary" inline-template>
           @include('paginas.home.vue.Contacto.Contacto_comun')
         </contacto-component>
