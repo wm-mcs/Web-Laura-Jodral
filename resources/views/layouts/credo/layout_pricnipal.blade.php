069817ac3147509978ef2207d3458ff01c6c12a0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-05 20:46:18 -0300

    sd

--- resources/views/layouts/credo/layout_pricnipal.blade.php
+++ resources/views/layouts/credo/layout_pricnipal.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Petit+Formal+Script&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{url()}}/fonts/icomoon/style.css">
     <link rel="stylesheet" href="{{url()}}/fonts/flaticon/font/flaticon.css">  
