fd15c7ee1525c95b06485665a81a650d58bfa333
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-09 18:38:45 -0300

    df

--- resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
+++ resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
@@ -14,31 +14,41 @@
           {{-- S e g u n d o   B l o q u e     --}}  
           <div class="col-md-3 p-4">
             <h3 class="footer-titulo-columna mb-5 text-color-primary text-bold">Rutas de interés</h3>
-            <ul class="list-unstyled">
+            
 
              
               
-                <li><a href="#contact-section" >Contacto</a></li>
+                <a href="#contact-section" >Contacto</a>
                @if(Auth::guest())
-                <li><a href="{{route('auth_login_get')}}" >Iniciar sesión</a></li>
+                <a href="{{route('auth_login_get')}}" >Iniciar sesión</a>
                @else
-                <li><a href="{{route('get_datos_corporativos')}}" >Administrar</a></li>
-                <li><a href="{{route('logout')}}" >Salir</a></li>
+                <a href="{{route('get_datos_corporativos')}}" >Administrar</a>
+                <a href="{{route('logout')}}" >Salir</a>
                @endif
-            </ul>
+            
           </div>   
 
 
           {{-- T e r c e r   B l o q u e     --}}  
           <div class="col-md-3 p-4">
             <h3 class="footer-titulo-columna mb-5 text-color-primary text-bold">Mis datos</h3>
-            <ul class="list-unstyled">                  
-              <li v-if="se_muestra(empresa.telefono)"><a  href="#" ><i class="fas fa-phone-square"></i>  @{{empresa.telefono}}</a></li>
-              <li v-if="se_muestra(empresa.celular)" ><a  href="#"  ><i class="fas fa-mobile-alt"></i> @{{empresa.celular}} </a></li>
-              <li v-if="se_muestra(empresa.direccion)"><a  href="#" ><i class="fas fa-map-marker-alt"></i> @{{empresa.direccion}}</a></li>
-              <li v-if="se_muestra(empresa.horarios)"><a  href="#" ><i class="far fa-clock"></i>@{{empresa.horarios}}</a></li>
-              <li v-if="se_muestra(empresa.email)"><a  href="#" ><i class="far fa-envelope"></i> @{{empresa.email}}</a></li>
-            </ul>
+                             
+              <p  v-if="se_muestra(empresa.telefono)"  > 
+                 <i class="fas fa-phone-square"></i>   @{{empresa.telefono}}
+              </p>
+              <p  v-if="se_muestra(empresa.celular)"   > 
+                 <i class="fas fa-mobile-alt"></i>     @{{empresa.celular}} 
+              </p>
+              <p  v-if="se_muestra(empresa.direccion)"  >
+                 <i class="fas fa-map-marker-alt"></i> @{{empresa.direccion}}
+              </p>
+              <p  v-if="se_muestra(empresa.horarios)"  > 
+                 <i class="far fa-clock"></i>          @{{empresa.horarios}}
+              </p>
+              <p  v-if="se_muestra(empresa.email)" >    
+                 <i class="far fa-envelope"></i>       @{{empresa.email}}
+              </p>
+            
           </div> 
         
                
@@ -47,12 +57,36 @@
           {{-- C u a r t o   B l o q u e     --}}
           <div class="col-md-3 p-4">
             <h3 class="footer-titulo-columna mb-5 text-color-primary text-bold">Sígueme</h3>
-            <a v-if="se_muestra(empresa.twitter_url)" :href="empresa.twitter_url" class="social-circle p-2"><span class="icon-twitter"></span></a>
-            <a v-if="se_muestra(empresa.facebook_url)" :href="empresa.facebook_url" class="social-circle p-2"><span class="icon-facebook"></span></a>
-            <a v-if=" se_muestra(empresa.instagram_url)" :href="empresa.instagram_url" class="social-circle p-2"><span class="icon-instagram"></span></a>
-            <a v-if="se_muestra(empresa.youtube_url)" :href="empresa.youtube_url" class="social-circle p-2"><span class="icon-youtube"></span></a>
-            <a v-if="se_muestra(empresa.linkedin_url)" :href="empresa.linkedin_url" class="social-circle p-2"><span class="icon-linkedin"></span></a>
-            <a v-if="se_muestra(empresa.Whatsapp_cel)" :href="empresa.link_whatsapp_send" class="social-circle p-2"> <i class="fab fa-whatsapp"></i></a>
+            <p v-if="se_muestra(empresa.twitter_url)"  class="">
+             <a :href="empresa.twitter_url">
+               <span class="icon-twitter mr-4"></span> Twitter
+             </a> 
+            </p>
+            <p v-if="se_muestra(empresa.facebook_url)"  class="">
+             <a :href="empresa.facebook_url">
+               <span class="icon-facebook mr-4"></span> Facebook
+             </a>
+            </p>
+            <p v-if=" se_muestra(empresa.instagram_url)"  class="">
+             <a :href="empresa.instagram_url">
+               <span class="icon-instagram mr-4"></span> Instagram
+             </a>
+            </p>
+            <p v-if="se_muestra(empresa.youtube_url)"  class="">
+             <a :href="empresa.youtube_url">
+               <span class="icon-youtube mr-4"></span> Youtube
+             </a>
+            </p>
+            <p v-if="se_muestra(empresa.linkedin_url)"  class="">
+             <a :href="empresa.linkedin_url">
+               <span class="icon-linkedin mr-4"></span> Linkedin
+             </a>
+            </p>
+            <p v-if="se_muestra(empresa.Whatsapp_cel)" class=""> 
+             <a :href="empresa.link_whatsapp_send" >
+               <i class="fab fa-whatsapp mr-4"></i> Whatsapp
+             </a>
+            </p>
           </div>
         </div>
 
