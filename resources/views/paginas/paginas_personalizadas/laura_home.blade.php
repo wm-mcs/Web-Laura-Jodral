345d303123aa074f2b8720b45eab400929456081
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-16 19:56:10 -0300

    gh

--- resources/views/paginas/paginas_personalizadas/laura_home.blade.php
+++ resources/views/paginas/paginas_personalizadas/laura_home.blade.php
@@ -166,13 +166,26 @@
         <div class="row d-flex flex-row justify-content-start">
           <div class="col-lg-6 ">
           <h2 class="h2-titulos-class mb-4 text-color-primary">¿Qué haré por tí?</h2>
-          <p class="parrafo-class mb-5"> Como facilitadora del desarrollo personal ,detectaré las áreas de dificultad que te impiden crecer personal y profesionalmente  y te asistiré en tus procesos de aprendizaje y cambio para conseguirlo.  Del mismo modo que si uno se adentra en un lugar desconocido a oscuras sintiéndose desorientado sin saber por dónde ir en ese momento, si tuviese una linterna a mano le ayudaría para orientarse, hay momentos en la vida en que gracias a esa ‘linterna’ podemos recorrer el camino que deseamos y decidimos realizar.
+          <p class="parrafo-class mb-5"> Como facilitadora del desarrollo personal ,<strong>detectaré las áreas de dificultad que te impiden crecer personal y profesionalmente  y te asistiré en tus procesos de aprendizaje y cambio para conseguirlo</strong> .  Del mismo modo que si uno se adentra en un lugar desconocido a oscuras sintiéndose desorientado sin saber por dónde ir en ese momento, si tuviese una linterna a mano le ayudaría para orientarse, <strong>hay momentos en la vida en que gracias a esa ‘linterna’ podemos recorrer el camino que deseamos y decidimos realizar</strong> .
           </p>   
 
-           <p class="parrafo-class m-0">
-        
-       Confío en cada uno <strong>puede llegar a ser la mejor versión de sí mismo</strong>  y la experiencia me ha mostrado que la gran mayoría de las personas viven distanciadas de sí mismas caminando por la vida como medianamente pueden sin ser quien realmente son con todo su ser y sin experimentar todas las potencialidades que tienen para manifestar en este mundo lo cual acarrea una serie de conflictos internos que si se dedicasen un poquito de tiempo para adentrarse y mirarse a  sí mismos podrían disfrutar de una vida más plena,  y <strong>que mejor momento que ahora, en esta Vida, en la que aún puedes hacer algo por ti</strong> .
-      </p> 
+          <p class="parrafo-class mb-5">
+            Me gustaría poder mostrarte las herramientas que te van cambiar la vida. Con el pasar de los años desarrollé la metodología justa y necesaría que te podrá ayudarte a tener el control sobre tu vida. Este proceso convina todo lo que he aprendido de: 
+          </p> 
+          <p>
+            <div class="lista-class">Terapia Gestalt</div>
+            <div class="lista-class">PNL ( Programación Neuro Lingüística)
+            </div>
+            <div class="lista-class">Astrologia Humanista
+            </div>
+          </p>
+          <a href="{{route('get_pagina_servicios')}}" class="Boton-Primario-Sin-Relleno Boton-Fuente-Chico"> Explora las herramientas que cambiarán tu vida 
+          <i class="fas fa-chevron-right"></i>
+          </a>
+
+
+
+          
 
 
           </div>
