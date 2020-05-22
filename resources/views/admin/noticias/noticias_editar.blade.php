fa13aab2608ae0e079d9920b04ee9310b1cb5e13
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-22 18:54:39 -0300

    ok

--- resources/views/admin/noticias/noticias_editar.blade.php
+++ resources/views/admin/noticias/noticias_editar.blade.php
@@ -4,14 +4,9 @@
 @section('miga-de-pan')
  
 
-  {{-- lugar atras --}}
-  <a href="{{route('get_admin_noticias')}}"><span>Noticias</span></a>
-
-  {{-- separador --}}
-  <span class="spam-separador"><span class="icon-keyboard_arrow_right"></span></span> 
-
-  {{-- lugar donde esta --}}
-  <span>Editar Noticia: {{$Entidad->name}}</span>
+ <h1 class="titulos-class mb-3  text-color-primary font-secondary">Editar</h1>
+ <p class="parrafo-class color-text-gris"> Link del post público <i class="fas fa-hand-point-right"></i> <a href="{{$Entidad->route}}"  target="_blank">{{$Entidad->name}}</a></p>
+  
 @stop
 
 @section('content')
@@ -24,31 +19,50 @@
                             'files' =>  true,
                             'id'    => 'form-admin-empresa-datos'
                           ])               !!}
-   <div class="formulario-contenedor">
+   <div class="row p-5">
 
       {{-- datos corporativos --}}
-      <div class="contenedor-grupo-datos">
-        <div class="contenedor-grupo-datos-titulo"><span class="icon-person"></span> Identidad</div>
+      <div class="contenedor-grupo-datos col-12 col-lg-6">
+        <div class="contenedor-grupo-datos-titulo"> Datos</div>
         <div class="contenedor-formulario-label-fiel">                       
-         @include('admin.noticias.formularios_partes.datos_basicos')
+          @include('admin.noticias.formularios_partes.datos_basicos')
         </div>
       </div>
 
+
       {{-- imagenes corporativos --}}
-      <div class="contenedor-grupo-datos">
-        <div class="contenedor-grupo-datos-titulo"><span class="icon-person"></span> Estado </div>
+      <div class="contenedor-grupo-datos col-12 col-lg-6">
+        <div class="contenedor-grupo-datos-titulo"> Imagen</div>
         <div class="contenedor-formulario-label-fiel">                       
           @include('admin.noticias.formularios_partes.datos_imagenes')
         </div>
       </div>
+      <div class="contenedor-grupo-datos col-12">
+        <div class="contenedor-grupo-datos-titulo">Contenido del post</div>
+        <div class="contenedor-formulario-label-fiel">                       
+          <div class="formulario-label-fiel">
+            @include('admin.noticias.formularios_partes.aclaracion_etiquetas')
+            {!! Form::label('description', 'Contenido', array('class' => 'formulario-label ')) !!}
+            {!! Form::textarea('description', null ,['class' => 'formulario-field',
+                                                     'rows' => 40 ]) !!}
+          </div>
+        </div>
+      </div>
 
       
 
-      
+     
    </div>
-   <div class="admin-boton-editar">
-     Editar Noticia
-   </div> 
+   
+    <div class="mt-5 mb-5 Boton-Fuente-Chica Boton-Primario-Relleno disparar-este-form">
+     Editar: {{$Entidad->name}}  <i class="fas fa-angle-double-right"></i>
+    </div> 
+
+      
+
+      
+   
+   
 
   {!! Form::close() !!}
 
