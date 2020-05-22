a5c6ae055ca2ed0224254c9f5055244ec8c01a7f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-22 19:18:15 -0300

    df

--- resources/views/admin/noticias/formularios_partes/datos_imagenes.blade.php
+++ resources/views/admin/noticias/formularios_partes/datos_imagenes.blade.php
@@ -12,20 +12,19 @@
 @if(isset($Entidad))
 <div class="row" >
 
-@if(file_exists($Entidad->path_url_img_portada))
-  <img class="admin-img-section-img" src="{{$Entidad->url_img_portada}}">
-@endif  
-@if(file_exists($Entidad->path_url_img_adicional))
-  <img class="admin-img-section-img" src="{{$Entidad->url_img_adicional}}">  
+		@if(file_exists($Entidad->path_url_img_portada))
+		<img class="admin-img-section-img" src="{{$Entidad->url_img_portada}}">
+		@endif  
+		@if(file_exists($Entidad->path_url_img_adicional))
+		<img class="admin-img-section-img" src="{{$Entidad->url_img_adicional}}">  
 
-@endif 
-@if(file_exists($Entidad->path_url_img_portada))
-<p class="p-2 mt-4 mb-3 text-center color-text-gris parrafo-class">
-	Para copiar la url de la imagen click derecho arriba de la misma y luego "Copiar dirección de la imagen".
-</p>
+		@endif 
+		@if(file_exists($Entidad->path_url_img_portada))
+		<p class="p-2 mt-4 mb-3 text-center color-text-gris parrafo-class">
+		Para copiar la url de la imagen click derecho arriba de la misma y luego "Copiar dirección de la imagen".
+		</p>
+		@endif
 </div>
-@endif
-
 @endif
 
 
