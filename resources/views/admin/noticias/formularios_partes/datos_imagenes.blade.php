ef0af428abae6b04ca3cfd712448b8ab0ec2b525
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-22 19:15:28 -0300

    df

--- resources/views/admin/noticias/formularios_partes/datos_imagenes.blade.php
+++ resources/views/admin/noticias/formularios_partes/datos_imagenes.blade.php
@@ -10,10 +10,21 @@
 </div> 
 
 @if(isset($Entidad))
-<div class="flex-row-column get_width_100" >
+<div class="row" >
+
+@if(file_exists($Entidad->path_url_img_portada))
   <img class="admin-img-section-img" src="{{$Entidad->url_img_portada}}">
+@endif  
+@if(file_exists($Entidad->path_url_img_adicional))
   <img class="admin-img-section-img" src="{{$Entidad->url_img_adicional}}">  
 </div>
+@endif 
+@if(file_exists($Entidad->path_url_img_portada))
+<p class="p-2 mt-4 mb-3 text-center color-text-gris parrafo-class">
+	Para copiar la url de la imagen click derecho arriba de la misma y luego "Copiar dirección de la imagen".
+</p>
+@endif
+
 @endif
 
 
