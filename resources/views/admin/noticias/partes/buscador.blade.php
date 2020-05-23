464f4be98b1320bc0d17e3127eb00145509fdaa3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-23 13:31:20 -0300

    df

--- resources/views/admin/noticias/partes/buscador.blade.php
+++ resources/views/admin/noticias/partes/buscador.blade.php
@@ -1,14 +1,14 @@
 {!! Form::model(Request::all(),['route'   => 'get_admin_noticias', 
                 'method'  => 'GET',
-                'class'   => 'col-6',
+                'class'   => 'col-6 col-lg-7',
                 'role'    => 'search' ])                             !!}
   <div class="col-12 d-flex align-items-center">
-  {!! Form::text('name', null , ['class'       => 'input-text-class-primary col-5', 
-                                 'placeholder' => 'Buscar'])        !!}
- 
+	  {!! Form::text('name', null , ['class'       => 'input-text-class-primary col-4', 
+	                                 'placeholder' => 'Buscar'])        !!}
+	 
 
-  <button type="submit" class="ml-3 sub-titulos-class text-color-primary col-1"><i class="fas fa-search"></i></button>
+	  <button type="submit" class="Boton-Fuente-Chica Boton-Primario-Sin-Relleno col-2">
+	    <i class="fas fa-search"></i>
+	  </button>
   </div>
-  
-
 {!! Form::close() !!}
