e9c426980fb63bfef38a3acb2797b7496291c6f7
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-23 13:30:13 -0300

    fg

--- resources/views/admin/noticias/noticias_home.blade.php
+++ resources/views/admin/noticias/noticias_home.blade.php
@@ -8,14 +8,19 @@
 @section('content')
 
  {{-- titulo --}}
- <div class="container p-3">
-    <div class="col-6"> 
+ <div class="container">
+  <div class="row  p-3">
+   
+
+
+    <div class="col-6 col-lg-4"> 
      <a class="col-12 Boton-Fuente-Chica Boton-Primario-Relleno" href="{{route('get_admin_noticias_crear')}}"> 
       Crear post
       
      </a>  
     </div>
     @include('admin.noticias.partes.buscador')
+   </div>
  </div>
  <div class="container p-4">
    <div class="row col-12">
