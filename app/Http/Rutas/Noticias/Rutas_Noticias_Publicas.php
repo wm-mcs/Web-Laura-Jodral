f7e14c424103f1cade5426c5a596e4497b3a9026
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-23 21:37:28 -0300

    df

--- app/Http/Rutas/Noticias/Rutas_Noticias_Publicas.php
+++ app/Http/Rutas/Noticias/Rutas_Noticias_Publicas.php
@@ -7,7 +7,7 @@ Route::get('/blog' , [
 );
      //Noticia Individual
       Route::get('/blog/{name}/{id}' , [                    
-        'uses' => 'Publicas\Paginas_Controller@get_pagina_noticia_individual',
+        'uses' => 'Publicas\Paginas_Controller@get_pagina_noticia_individual44',
         'as'   => 'get_pagina_noticia_individual']
       )->where(['id'  => '[0-9]+',
                 'name'=> '^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$']);
