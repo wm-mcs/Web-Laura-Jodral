4f81f9dd543af128f155453598113c6c5bf53958
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-23 22:26:47 -0300

    df

--- app/Http/Rutas/Publicas.php
+++ app/Http/Rutas/Publicas.php
@@ -6,7 +6,12 @@
 require __DIR__ . '/Formularios/Rutas_Formularios_Publicas.php';
 
 
-
+// N o t i c i a   I n d i v i d u a l 
+Route::get('/blog/{name}-{id}' , [                    
+'uses' => 'Publicas\Paginas_Controller@get_pagina_noticia_individual',
+'as'   => 'get_pagina_noticia_individual']
+)/*->where(['id'  => '[0-9]+',
+        'name'=> '^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$'])*/;
 
 
 
@@ -34,13 +39,6 @@ Route::get('/Quién-es-Laura-Jodral-Garcia' , [
   'uses' => 'Publicas\Paginas_Controller@get_pagina_quien_es',
   'as'   => 'get_pagina_quien_es']
 );
-
-// N o t i c i a   I n d i v i d u a l 
-Route::get('/blog/{name}-{id}' , [                    
-'uses' => 'Publicas\Paginas_Controller@get_pagina_noticia_individual',
-'as'   => 'get_pagina_noticia_individual']
-)->where(['id'  => '[0-9]+',
-        'name'=> '^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$']);
 
 
 
