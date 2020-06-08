978d7e3767e2889e9f8eeb433276d7f12eb7d2de
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-08 13:40:14 -0300

    gh

--- resources/views/paginas/paginas_personalizadas/laura_home.blade.php
+++ resources/views/paginas/paginas_personalizadas/laura_home.blade.php
@@ -170,7 +170,7 @@
 
 
           </div>
-          <div class="col-12  p-2 p-lg-4 mt-3 mt-lg-0 " >
+          <div class="col-12  col-lg-6 p-2 p-lg-4 mt-3 mt-lg-0 " >
             <img calss="img-fluid" src="{{url()}}/imagenes/Home/laura_jodral_vive_la_vida_que_amas.jpg">
             
           </div>
