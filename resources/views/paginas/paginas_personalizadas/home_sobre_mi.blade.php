96cb552dc9b632a0a401ba8f9fcaf4362286329c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-25 12:40:39 -0300

    gh

--- resources/views/paginas/paginas_personalizadas/home_sobre_mi.blade.php
+++ resources/views/paginas/paginas_personalizadas/home_sobre_mi.blade.php
@@ -12,7 +12,7 @@
             <p class="parrafo-class mb-5">
              <strong> Laura Jodral García es terapeuta Gestalt y tiene un Master en PNL</strong>. Se ha formado en Astrología Humanista y hoy en día se dedica a acompañar a personas  que desean optimizar y tomar las riendas de su vida a conseguir aquello que desean ejecutando las acciones apropiadas y adecuadas para que puedan llevar la vida que desean. Su incansable búsqueda sobre el sentido de la Vida la han llevado a centrarse en el crecimiento personal y siente que ha encontrado su misión en esta vida.                
             </p>
-            <p class="parrafo-class mb-5 font-primary">
+            <p class="psub-titulos-class mb-0 font-primary">
                "Me lo contaron y lo olvidé, lo vi y lo entendí, lo hice y lo aprendí".
             </p>
             <p>
