a8eac22f9db1254145766231e672db39b8c993a9
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-13 14:22:50 -0300

    fg

--- resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
+++ resources/views/paginas/paginas_personalizadas/Footer/Footer_principal.blade.php
@@ -106,7 +106,7 @@
        
       </div>
     </footer>
-     <div class="p-5 background-gris-4">
+     <div class="p-5 background-gris-5">
       <p class="m-0 text-center color-text-white parrafo-class"> 
         <small>
          <span class="color-text-white">Todos los derechos reservados <b class="text-color-primary">@{{empresa.name}}</b> © 2020</span> 
